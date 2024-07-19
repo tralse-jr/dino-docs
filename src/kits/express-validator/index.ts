109b4bfe78698af03b3069516e202dc46c5a6c72
kenneth <kennethbelardo55@gmail.com>
2024-07-19 18:01:15 +0800

    refactor: Add error catching, add logging for load success and error.

--- src/kits/express-validator/index.ts
+++ src/kits/express-validator/index.ts
@@ -1,4 +1,7 @@
 import { Fossils } from "../../types";
+import { err, log } from "../../utils/consoleUtils";
+import { isValidMethod, Methods } from "../../validators/methodValidator";
+import { isValidRoutePath } from "../../validators/routeValidator";
 import { fossilToExpressValidator } from "./converter";
 import { Express } from "express";
 
@@ -9,23 +12,37 @@ import { Express } from "express";
  * and an array of Fossils. It generates middleware for each Fossil and
  * applies it to the corresponding route on the Express app.
  *
- * @experimental
  *
  * @param app - The Express application instance.
  * @param fossils - An array of Fossils containing the route information and validators.
  * @param baseRoutePath - The base path for the routes.
  */
+
 const fossilsToExpressMiddleware = (
   app: Express,
   fossils: Fossils[],
   baseRoutePath: string = ""
 ) => {
-  fossils.forEach((fossil) => {
-    const middleware = fossilToExpressValidator(fossil);
-    app[
-      fossil.method.toLowerCase() as "get" | "post" | "put" | "patch" | "delete"
-    ](`${baseRoutePath}${fossil.path}`, ...middleware);
-  });
+  try {
+    fossils.forEach((fossil) => {
+      const middleware = fossilToExpressValidator(fossil);
+      const method = isValidMethod(fossil.method) as Methods | undefined;
+      const path = `${baseRoutePath}${fossil.path}`;
+
+      if (!method)
+        throw new Error(
+          `In ${path}: Invalid method ${fossil.method.toLowerCase()}.`
+        );
+
+      if (!isValidRoutePath(path))
+        throw new Error(`In ${path}: Invalid path ${path}.`);
+
+      app[method](path, ...middleware);
+      log(`In ${path}: Validation middleware succesfully injected.`);
+    });
+  } catch (error: any) {
+    err(`Error: ${error.message}`);
+  }
 };
 
 export { ERROR_CODES } from "./error-codes";
